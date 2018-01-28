a4c8ed10f509fa67acb21f855a524e9b5109ab1b
Charles Comstock <dgtized@gmail.com>
2018-01-28 04:21:06 -0600

    fill the available space and bump to 8k

--- src/redwall/core.cljs
+++ src/redwall/core.cljs
@@ -3,17 +3,16 @@
 
 (enable-console-print!)
 
-(def constants { :cells 2048})
+(def constants {:cells 8000})
 
 (defn render-machine []
   (let [cells (constants :cells)
-        box 96
-        c 128
+        box 128
         edge 0.05
         size (- 1.0 edge)]
-    [:svg {:view-box (str "0 0 " box " " box) :width 1280 :height 768}
+    [:svg {:view-box (str "0 0 " box " " box)}
      (for [i (range cells)
-           :let [x (mod i c) y (quot i c)] ]
+           :let [x (mod i box) y (quot i box)] ]
        [:rect {:x (+ x edge) :y (+ y edge)
                :width size :height size
                :key (str "m" i)
