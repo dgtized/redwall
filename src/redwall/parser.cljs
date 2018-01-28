7a6ad0de15d0436a5cef97400235eaf52a62a636
Charles Comstock <dgtized@gmail.com>
2018-01-28 02:37:33 -0600

    add alternate versions of imp and dwarf + sleepy

--- src/redwall/parser.cljs
+++ src/redwall/parser.cljs
@@ -11,12 +11,18 @@
 
 (def dwarf (slurp "resources/dwarf.asm"))
 (def dwarf-2 (slurp "resources/dwarf-2.asm"))
+(def dwarf-3 (slurp "resources/dwarf-3.asm"))
 (def imp (slurp "resources/imp.asm"))
+(def imp-2 (slurp "resources/imp-2.asm"))
+(def sleepy (slurp "resources/sleepy.asm"))
 
 (comment
   (redcode "; comment\nMOV 0, 1")
   (redcode "MOV 0, 1")
   (redcode dwarf)
   (redcode dwarf-2)
-  (redcode imp))
+  (redcode dwarf-3)
+  (redcode imp)
+  (redcode imp-2)
+  (redcode sleepy))
 
