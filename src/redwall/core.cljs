223d9e82452e9d819fdf8674e2ed8f4add9ab596
Charles Comstock <dgtized@gmail.com>
2018-02-07 15:48:55 -0600

    WIP

--- src/redwall/core.cljs
+++ src/redwall/core.cljs
@@ -4,19 +4,29 @@
 (enable-console-print!)
 
 (def constants {:cells 8000})
+(defonce machine (r/atom {:memory (make-array Integer/TYPE (constants :cells))}))
+
+(defn show-memory [e]
+  (println (-> e .-currentTarget .-key)))
+
+(defn program-color [instr]
+  :lightgrey)
 
 (defn render-machine []
   (let [cells (constants :cells)
+        memory (:memory @machine)
         box 128
         edge 0.05
         size (- 1.0 edge)]
-    [:svg {:view-box (str "0 0 " box " " box)}
+    [:svg {:view-box (str "0 0 " box " " box)
+           :on-mouse-over show-memory}
      (for [i (range cells)
            :let [x (mod i box) y (quot i box)] ]
        [:rect {:x (+ x edge) :y (+ y edge)
                :width size :height size
                :key (str "m" i)
-               :fill :grey}])]))
+               :fill (program-color
+                      (nth memory i))}])]))
 
 (defn render []
   [:div
